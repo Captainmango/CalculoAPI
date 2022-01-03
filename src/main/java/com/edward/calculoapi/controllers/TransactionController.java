3b1bb88e14a693ae635ed33fcfd62599da19745d
Edward Heaver <edward.heaver01@gmail.com>
2022-01-03 23:20:31 +0000

    Removed uneeded imports

--- src/main/java/com/edward/calculoapi/controllers/TransactionController.java
+++ src/main/java/com/edward/calculoapi/controllers/TransactionController.java
@@ -3,32 +3,15 @@ package com.edward.calculoapi.controllers;
 import com.edward.calculoapi.database.dto.requests.CreateTransactionRequest;
 import com.edward.calculoapi.database.dto.requests.UpdateTransactionRequest;
 import com.edward.calculoapi.models.Transaction;
-import com.edward.calculoapi.security.services.AuthenticationFacade;
 import com.edward.calculoapi.security.services.AuthenticationFacadeImpl;
 import com.edward.calculoapi.security.services.UserDetailsImpl;
-import com.edward.calculoapi.serializers.TransactionSerializer;
 import com.edward.calculoapi.services.TransactionCRUDService;
-import com.fasterxml.jackson.core.FormatSchema;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import jdk.jfr.ContentType;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin(origins = "${edward.app.requestOrigin}", maxAge = 3600)
