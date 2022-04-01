07b358b60023eb1c700b3dd653f2b190d64c6647
Edward Heaver <edward.heaver01@gmail.com>
2022-04-01 09:29:27 +0100

    fixing migration and props

--- src/main/resources/db/migration/V1__Init_Database.sql
+++ src/main/resources/db/migration/V1__Init_Database.sql
@@ -21,7 +21,7 @@ CREATE TABLE transactions (
 	CONSTRAINT transactions_pkey PRIMARY KEY (id)
 );
 
-ALTER TABLE transactions ADD CONSTRAINT fkqwv7rmvc8va8rep7piikrojds FOREIGN KEY (user_id) REFERENCES public.users(id);
+ALTER TABLE transactions ADD CONSTRAINT fkqwv7rmvc8va8rep7piikrojds FOREIGN KEY (user_id) REFERENCES users(id);
 
 CREATE TABLE categories (
 	id bigserial NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE refreshtoken (
 	CONSTRAINT uk_or156wbneyk8noo4jstv55ii3 UNIQUE (token)
 );
 
-ALTER TABLE refreshtoken ADD CONSTRAINT fka652xrdji49m4isx38pp4p80p FOREIGN KEY (user_id) REFERENCES public.users(id);
+ALTER TABLE refreshtoken ADD CONSTRAINT fka652xrdji49m4isx38pp4p80p FOREIGN KEY (user_id) REFERENCES users(id);
 
 CREATE TABLE transaction_categories (
 	transaction_id int8 NOT NULL,
@@ -52,8 +52,8 @@ CREATE TABLE transaction_categories (
 	CONSTRAINT transaction_categories_pkey PRIMARY KEY (transaction_id, category_id)
 );
 
-ALTER TABLE transaction_categories ADD CONSTRAINT fk4w5cqbyyhe713v88k32809a5i FOREIGN KEY (transaction_id) REFERENCES public.transactions(id);
-ALTER TABLE transaction_categories ADD CONSTRAINT fkl8nhemttklbafivosqgyv35rl FOREIGN KEY (category_id) REFERENCES public.categories(id);
+ALTER TABLE transaction_categories ADD CONSTRAINT fk4w5cqbyyhe713v88k32809a5i FOREIGN KEY (transaction_id) REFERENCES transactions(id);
+ALTER TABLE transaction_categories ADD CONSTRAINT fkl8nhemttklbafivosqgyv35rl FOREIGN KEY (category_id) REFERENCES categories(id);
 
 CREATE TABLE user_roles (
 	user_id int8 NOT NULL,
@@ -61,5 +61,5 @@ CREATE TABLE user_roles (
 	CONSTRAINT user_roles_pkey PRIMARY KEY (user_id, role_id)
 );
 
-ALTER TABLE user_roles ADD CONSTRAINT fkh8ciramu9cc9q3qcqiv4ue8a6 FOREIGN KEY (role_id) REFERENCES public.roles(id);
-ALTER TABLE user_roles ADD CONSTRAINT fkhfh9dx7w3ubf1co1vdev94g3f FOREIGN KEY (user_id) REFERENCES public.users(id);
+ALTER TABLE user_roles ADD CONSTRAINT fkh8ciramu9cc9q3qcqiv4ue8a6 FOREIGN KEY (role_id) REFERENCES roles(id);
+ALTER TABLE user_roles ADD CONSTRAINT fkhfh9dx7w3ubf1co1vdev94g3f FOREIGN KEY (user_id) REFERENCES users(id);
